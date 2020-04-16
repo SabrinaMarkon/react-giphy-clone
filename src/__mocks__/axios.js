2c27cb7744ba7e72929b7ebc80dbf7d3b08de9d1
Sabrina Markon <phpsitescripts@outlook.com>
2020-04-16 17:16:18 -0600

    Set up Jest to mock Axios.

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,19 @@
+// Set up Jest to mock Axios.
+
+/*
+mockResolvedValue:
+
+Syntactic sugar function for:
+jest.fn().mockImplementation(() => Promise.resolve(value));
+
+Useful to mock async functions in async tests:
+test('async test', async () => {
+  const asyncMock = jest.fn().mockResolvedValue(43);
+
+  await asyncMock(); // 43
+});
+*/
+export default {
+    get: jest.fn().mockResolvedValue({ data: {} })
+};
+
